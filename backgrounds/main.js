c2f0836b1a6062df31869bdf941e1f4710e87c33
eryw <kristeryw@gmail.com>
2017-08-10 20:55:19 +0700

    Follow standardjs conding standard

--- backgrounds/main.js
+++ backgrounds/main.js
@@ -1,97 +1,97 @@
-"use strict";
+'use strict'
 
-var debug = false;
-var mode = 0;
+var debug = false
+var mode = 0
 
-let regexes = [];
+let regexes = []
 
-function normalizeData(data) {
-    if (!data.simpleBlocker) {
-        data = {
-            simpleBlocker: {
-                regexes: '',
-                debug: debug,
-                mode: mode
-            }
-        };
-    }
-
-    return data;
-}
-
-function updateCacheVar(strRegexArray, lmode, ldebug) {
-    mode = lmode;
-    debug = ldebug;
-    regexes = [];
-    for (let r of strRegexArray) {
-        if (r) {
-            try {
-              regexes.push(new RegExp(r));
-            } catch(e) {
-              // silent
-            }
-        }
-    }
-}
-
-function save(stringRegexes, lmode, ldebug) {
-    browser.storage.local.get("simpleBlocker").then((data) => {
-        data = normalizeData(data);
-        data.simpleBlocker.regexes = stringRegexes;
-        data.simpleBlocker.mode = lmode;
-        data.simpleBlocker.debug = ldebug;
-        browser.storage.local.set(data);
-        updateCacheVar(
-          stringRegexes.split(/\r?\n/),
-          lmode,
-          ldebug
-          );
-    });
-}
-
-function retrieve() {
-    return browser.storage.local.get("simpleBlocker").then((data) => {
-        data = normalizeData(data);
-        updateCacheVar(
-          data.simpleBlocker.regexes.split(/\r?\n/),
-          data.simpleBlocker.mode,
-          data.simpleBlocker.debug
-          );
-
-        return data.simpleBlocker;
-    });
-}
-
-function filterRequest(request) {
-    let cancel = false;
-    for (let regex of regexes) {
-      if ((mode == '0' && regex.test(request.url)) ||
-          (mode == '1' && !regex.test(request.url))
-      ) {
-        if (debug == '1') {
-          console.log("Canceled request: " + request.url);
-        }
-        cancel = true;
-        break;
+function normalizeData (data) {
+  if (!data.simpleBlocker) {
+    data = {
+      simpleBlocker: {
+        regexes: '',
+        debug: debug,
+        mode: mode
       }
     }
+  }
 
-    return { cancel: cancel };
+  return data
 }
 
-browser.storage.local.get("simpleBlocker").then((data) => {
-    data = normalizeData(data);
+function updateCacheVar (strRegexArray, lmode, ldebug) {
+  mode = lmode
+  debug = ldebug
+  regexes = []
+  for (let r of strRegexArray) {
+    if (r) {
+      try {
+        regexes.push(new RegExp(r))
+      } catch (e) {
+        // silent
+      }
+    }
+  }
+}
+
+function save (stringRegexes, lmode, ldebug) { // eslint-disable-line no-unused-vars
+  browser.storage.local.get('simpleBlocker').then((data) => {
+    data = normalizeData(data)
+    data.simpleBlocker.regexes = stringRegexes
+    data.simpleBlocker.mode = lmode
+    data.simpleBlocker.debug = ldebug
+    browser.storage.local.set(data)
+    updateCacheVar(
+      stringRegexes.split(/\r?\n/),
+      lmode,
+      ldebug
+    )
+  })
+}
+
+function retrieve () { // eslint-disable-line no-unused-vars
+  return browser.storage.local.get('simpleBlocker').then((data) => {
+    data = normalizeData(data)
     updateCacheVar(
       data.simpleBlocker.regexes.split(/\r?\n/),
       data.simpleBlocker.mode,
       data.simpleBlocker.debug
-      );
+    )
 
-    //browser.storage.local.remove("simpleBlocker")
-});
+    return data.simpleBlocker
+  })
+}
+
+function filterRequest (request) {
+  let cancel = false
+  for (let regex of regexes) {
+    if ((mode === '0' && regex.test(request.url)) ||
+      (mode === '1' && !regex.test(request.url))
+    ) {
+      if (debug === '1') {
+        console.log('Canceled request: ' + request.url)
+      }
+      cancel = true
+      break
+    }
+  }
+
+  return { cancel: cancel }
+}
+
+browser.storage.local.get('simpleBlocker').then((data) => {
+  data = normalizeData(data)
+  updateCacheVar(
+    data.simpleBlocker.regexes.split(/\r?\n/),
+    data.simpleBlocker.mode,
+    data.simpleBlocker.debug
+  )
+
+  // browser.storage.local.remove("simpleBlocker")
+})
 
 browser.webRequest.onBeforeRequest.addListener(
-    filterRequest,
-    {urls: ["<all_urls>"]},
-    ["blocking"]
-);
+  filterRequest,
+  { urls: ['<all_urls>'] },
+  ['blocking']
+)
--- options/options.js
+++ options/options.js
@@ -1,24 +1,24 @@
-"use strict";
+'use strict'
 
-let background = browser.extension.getBackgroundPage();
+let background = browser.extension.getBackgroundPage()
 
 document.addEventListener('DOMContentLoaded', function () {
-    background.retrieve().then((data) => {
-      document.querySelector('#regexes').value = data.regexes;
-      document.querySelector('input[name="mode"][value="' + data.mode + '"]').checked = true;
-      document.querySelector('input[name="debugging"][value="' + data.debug + '"]').checked = true;
-    });
-});
+  background.retrieve().then((data) => {
+    document.querySelector('#regexes').value = data.regexes
+    document.querySelector('input[name="mode"][value="' + data.mode + '"]').checked = true
+    document.querySelector('input[name="debugging"][value="' + data.debug + '"]').checked = true
+  })
+})
 
 document.querySelector('#regexes').addEventListener('keydown', function (e) {
-    document.querySelector('aside').classList.remove('visible');
-});
+  document.querySelector('aside').classList.remove('visible')
+})
 
 document.querySelector('#submit').addEventListener('click', function (e) {
-    e.preventDefault();
-    background.save(document.querySelector('#regexes').value,
-      document.querySelector('input[name="mode"]:checked').value,
-      document.querySelector('input[name="debugging"]:checked').value
-      );
-    document.querySelector('aside').classList.add('visible');
-});
+  e.preventDefault()
+  background.save(document.querySelector('#regexes').value,
+    document.querySelector('input[name="mode"]:checked').value,
+    document.querySelector('input[name="debugging"]:checked').value
+  )
+  document.querySelector('aside').classList.add('visible')
+})
